17fb9bf3de7d6bd058fec0588c1be27030cb5e48
[Ruchi Dewangan] <[ruchidew11@gmail.com]>
2015-12-19 16:01:06 -0800

    updating

---
+++ src/main/java/com/sqa/rd/employees/IPerson.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: IPerson.java<br>
+ * Dewangan, Ruchi<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Dec 19, 2015
+ */
+package com.sqa.rd.employees;
+
+/**
+ * IPerson //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Dewangan, Ruchi
+ * @version 1.0.0
+ * @since 1.0
+ */
+public interface IPerson {
+
+	final static int MAX_AGE = 110;
+
+	public String getName();
+
+	public boolean move();
+}
